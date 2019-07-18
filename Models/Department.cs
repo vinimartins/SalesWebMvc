65491eb1dd52266f3c82eaad101789dfcbf7c355
Vinicius Manara <vinimanara@hotmail.com>
2019-07-18 11:44:12 -0300

    Add entities

--- Models/Department.cs
+++ Models/Department.cs
@@ -3,11 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace SalesWebMvc.Models {
     public class Department {
 
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller> ( );
 
+
+        public Department ( )
+            {
+
+            }
+
+        public Department ( int id, string name )
+            {
+            Id = id;
+            Name = name;
+            }
+
+        public double TotalSales(DateTime initial, DateTime final )
+            {
+            return Sellers.Sum ( seller => seller.TotalSales ( initial, final ) );
+            }
         }
     }
---
+++ Models/SalesRecord.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models
+    {
+    public class SalesRecord
+        {
+
+        public int ID { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public double BaseSalary { get; set; }
+        public Seller Seller { get; set; }
+
+        public SalesRecord ( )
+            {
+            }
+
+        public SalesRecord ( int iD, DateTime date, double amount, double baseSalary, Seller seller )
+            {
+            ID = iD;
+            Date = date;
+            Amount = amount;
+            BaseSalary = baseSalary;
+            Seller = seller;
+            }
+        }
+    }
