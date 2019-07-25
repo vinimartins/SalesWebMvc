2869a2b29ed4cf62f57773504f4adca79d196484
Vinicius Manara <vinimanara@hotmail.com>
2019-07-25 11:54:24 -0300

    Delete Seller

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -1,22 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 using SalesWebMvc.Models;
 using SalesWebMvc.Models.ViewModels;
 using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
-{
-    public class SellersController : Controller
     {
+    public class SellersController:Controller
+        {
         private readonly SellerService _sellerService;
         private readonly DepartmentService _departmentService;
-      
+
         //construtor pra atribuir o valor do sellerservice
-        public SellersController (SellerService sellerService , DepartmentService departmentService)
+        public SellersController( SellerService sellerService, DepartmentService departmentService )
             {
 
             _sellerService = sellerService;
@@ -25,30 +21,56 @@ namespace SalesWebMvc.Controllers
             }
 
         //chamando o que está no serviço "SellerService"    
-        public IActionResult Index()
-        {
+        public IActionResult Index( )
+            {
             //retornando lista de sellers
-            var list = _sellerService.FindAll ( );
-            return View(list);
-        }
+            var list = _sellerService.FindAll( );
+            return View( list );
+            }
 
         public IActionResult Create( )
             {
-             
-            var departments = _departmentService.FindAll();
-            var viewModel = new SellerFormViewModel { Departments =  departments };
-            
-            return View(viewModel);
+
+            var departments = _departmentService.FindAll( );
+            var viewModel = new SellerFormViewModel { Departments = departments };
+
+            return View( viewModel );
 
             }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Seller seller)
+        public IActionResult Create( Seller seller )
             {
             _sellerService.Insert( seller );
             return RedirectToAction( nameof( Index ) );
 
             }
+
+        public IActionResult Delete( int? id )
+            {
+            if(id == null)
+                {
+                return NotFound( );
+                }
+            var obj = _sellerService.FindById( id.Value );
+            if(obj == null)
+                {
+                return NotFound( );
+                }
+
+            return View( obj );
+
+            }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete( int id )
+            {
+            _sellerService.Remove( id );
+            return RedirectToAction( nameof( Index ) );
+
+            }
+
+        }
     }
-}
--- Services/SellerService.cs
+++ Services/SellerService.cs
@@ -1,8 +1,6 @@
 ﻿using SalesWebMvc.Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace SalesWebMvc.Services
     {
@@ -13,20 +11,32 @@ namespace SalesWebMvc.Services
         private readonly SalesWebMvcContext _context;
 
 
-        public SellerService(SalesWebMvcContext context )
+        public SellerService( SalesWebMvcContext context )
             {
             _context = context;
             }
 
         //retornar lista com todos sellers
-        public List<Seller> FindAll ( )
+        public List<Seller> FindAll( )
             {
-            return _context.Seller.ToList ( );
-            } 
+            return _context.Seller.ToList( );
+            }
 
-        public void Insert(Seller obj )
+        public void Insert( Seller obj )
             {
             _context.Add( obj );
+            _context.SaveChanges( );
+            }
+
+        public Seller FindById( int id )
+            {
+            return _context.Seller.FirstOrDefault( obj => obj.Id == id );
+            }
+
+        public void Remove( int id )
+            {
+            var obj = _context.Seller.Find( id );
+            _context.Seller.Remove( obj );
             _context.SaveChanges( );
             }
 
