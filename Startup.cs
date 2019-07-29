1b43583b6fee19a2acfd640b23d9a8b32bf15629
Vinicius Manara <vinimanara@hotmail.com>
2019-07-29 14:26:52 -0300

    App Locale, number and date formating

--- Startup.cs
+++ Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Globalization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
@@ -30,6 +32,7 @@ namespace SalesWebMvc
             {
             services.Configure<CookiePolicyOptions> ( options =>
             {
+
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
@@ -55,6 +58,17 @@ namespace SalesWebMvc
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure ( IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService )
             {
+
+            var enUS = new CultureInfo( "en-US" );
+            var localizationOptions = new RequestLocalizationOptions
+                {
+                DefaultRequestCulture = new RequestCulture( enUS ),
+                SupportedCultures = new List<CultureInfo> { enUS },
+                SupportedUICultures = new List<CultureInfo> { enUS }
+                };
+
+            app.UseRequestLocalization( localizationOptions );
+
             if ( env.IsDevelopment ( ) )
                 {
                 app.UseDeveloperExceptionPage ( );
